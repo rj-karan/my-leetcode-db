c65c9cd9e7a68597b09259a92eab18936995d7fe
KARAN RJ <rjkaran418@gmail.com>
2026-06-24 10:27:07 +0530

    Time: 0 ms (100%), Space: 19.5 MB (7.25%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -3,7 +3,7 @@ class Solution:
         i=0
         j=0
         while i<len(s) and j<len(t):
-            if s[i]==t[j]:
+            if  s[i]==t[j]:
                 i+=1
             j+=1
         return i==len(s)
